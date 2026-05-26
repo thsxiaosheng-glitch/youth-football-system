a6735aba1b072947747c237651b49c5b32a0623b
thsxiaosheng-glitch <thsxiaosheng@gmail>
2026-05-26 20:06:20 +0800

    fix prisma export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma =
-  global.prisma ||
+const prisma =
+  globalForPrisma.prisma ??
   new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+  globalForPrisma.prisma = prisma;
 }
+
+export default prisma;
